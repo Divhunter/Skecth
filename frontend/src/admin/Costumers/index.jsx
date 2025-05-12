bc072b744ba10777fbe8745b1a1b6796611cfaba
aaron <aarondivi@gmail.com>
2025-05-12 02:22:41 +0200

    init

--- frontend/src/admin/Costumers/index.jsx
+++ frontend/src/admin/Costumers/index.jsx
@@ -148,7 +148,7 @@ const Costumers = () => {
 
   // Gérer la suppression de tous les projets
   const handleDeleteAllProjets = () => {
-    selectedProjetIds?.map((id) => handleDeleteProjet(id))
+    selectedProjetIds?.forEach((id) => handleDeleteProjet(id))
     setShowModalConfirm(!showModalConfirm)
     setSelectedProjetIds([])
     setSelectMultipl(!selectMultipl)
