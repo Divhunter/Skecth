d5c70ddf1c737d18cf99e930e9f2c61890be67ad
aaron <aarondivi@gmail.com>
2025-06-01 22:42:02 +0200

    init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import ScrollToTop from './functions/ScrollToTop'
 import { Routes, Route } from 'react-router-dom'
 import SketchBanner from './pages/SketchBanner'
@@ -7,37 +7,32 @@ import SketchHeader from './pages/SketchHeader'
 import SketchFooter from './pages/SketchFooter'
 import SketchInfo from './pages/SketchInfo'
 import SketchTchat from './pages/SketchTchat'
-import  CGU from './pages/CGU'
+import CGU from './pages/CGU'
 import Error from './_utils/Error'
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-// styles
 import './styles/m-app.css'
 import './styles/t-d-app.css'
 
 const App = () => {
-
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    }, [])
-
-    return (
-        <div>
-            <SketchHeader />
-                <Routes>
-                    <Route exact path='/' element={ <SketchBanner /> } />
-                    <Route path='/SketchInfo' element={ <SketchInfo /> } />
-                    <Route path='/SketchCrea' element={ <SketchCrea /> } />
-                    <Route path='/SketchTchat' element={ <SketchTchat /> } />
-                    <Route path='/CGU' element={ <CGU /> } />
-                    <Route path='/*' element={ <Error /> } />
-                </Routes>
-            <SketchFooter />
-            <ScrollToTop />
-        </div>
-    )
+  return (
+    <>
+      <SketchHeader />
+      <ScrollToTop />
+      <Routes>
+        <Route path='/' element={<SketchBanner />} />
+        <Route path='/SketchInfo' element={<SketchInfo />} />
+        <Route path='/SketchCrea' element={<SketchCrea />} />
+        <Route path='/SketchTchat' element={<SketchTchat />} />
+        <Route path='/CGU' element={<CGU />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <SketchFooter />
+      <ToastContainer />
+    </>
+  )
 }
 
 export default App
