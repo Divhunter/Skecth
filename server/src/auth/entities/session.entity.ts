8a58066167887dd4e95cfa6a7caec88aa85442f9
aaron <aarondivi@gmail.com>
2025-06-01 22:11:05 +0200

    page

--- server/src/auth/entities/session.entity.ts
+++
@@ -1,9 +0,0 @@
-export interface SessionModel {
-  id: string;
-  sessionToken: string;
-  userId: string;
-  expires: Date;
-  loginIp?: string | null;
-  deviceInfo?: string | null;
-  isLoggedIn?: boolean | null;
-}
--- server/src/projet/entities/message.model.ts
+++
@@ -1,6 +0,0 @@
-export class Message {
-  id: string;
-  content: string;
-  createdDate: Date;
-  projetId: string;
-}
